6e2ce469de44e2a79677106d813fd4774f96d3f1
qdequele <github@dequelen.me>
2016-10-21 13:15:23 +0200

    continue

--- srcs/shell/shell.c
+++ srcs/shell/shell.c
@@ -6,7 +6,7 @@
 /*   By: qdequele <qdequele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/02 15:21:13 by qdequele          #+#    #+#             */
-/*   Updated: 2016/10/21 12:55:16 by qdequele         ###   ########.fr       */
+/*   Updated: 2016/10/21 13:09:14 by qdequele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,37 +21,46 @@ int		shell_core(t_list **env, char **cmds)
 }
 
 
-void		shell_exec_line(char *cmd)
+int		shell_exec_line(char *cmd)
+{
+	t_cmd 	*cmd;
+	t_list	*env;
+	char	**cmds;
+
+	env = g_env;
+	cmds = ft_str_to_tab(cmd);
+	if (cmds[0][0] == '!')
+	{
+		cmds[0] = action_seek_to_history(cmds[0]);
+		if (cmds[0] == NULL)
+			return (0);
+	}
+	if ((cmd = parse_cmd(cmd)) && builtins_find(cmd) == 0)
+		return (exec_cmd(cmd));
+	else
+		return (shell_core(&env, cmds));
+}
+
+int 		shell_parse_or_line(char *cmd)
 {
 
 }
 
-int 		shell_parse_line(char *line)
+int 		shell_parse_and_line(char *cmd)
 {
-	char	**cmds;
+
+}
+
+int 		shell_parse_semicolon_line(char *line)
+{
 	char	**l_cmd;
-	t_cmd 	*cmd;
-	t_list	*env;
 	int		i;
 
-	env = g_env;
 	i = 0;
 	l_cmd = ft_strsplit(line, ';');
 	while (l_cmd[i])
 	{
-		cmds = ft_str_to_tab(l_cmd[i]);
-		if (cmds[0][0] == '!')
-		{
-			cmds[0] = action_seek_to_history(cmds[0]);
-			if (cmds[0] == NULL)
-				return (0);
-		}
-		if ((cmd = parse_cmd(l_cmd[i])) && builtins_find(l_cmd[i]) == 0)
-		{
-			exec_cmd(cmd);
-		}
-		else
-			shell_core(&env, cmds); //TODO : Starting here
+		shell_exec_line(cmds, l_cmd[i]);
 		i++;
 	}
 }
