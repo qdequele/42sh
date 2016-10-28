70a4b5efd888a9948d560348a4840778c426e861
qdequele <github@dequelen.me>
2016-10-28 17:50:22 +0200

    close fd after use

--- libft/ft_utils/ft_console.c
+++ libft/ft_utils/ft_console.c
@@ -6,46 +6,49 @@
 /*   By: qdequele <qdequele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 16:05:41 by qdequele          #+#    #+#             */
-/*   Updated: 2016/10/26 19:39:27 by qdequele         ###   ########.fr       */
+/*   Updated: 2016/10/28 17:50:08 by qdequele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-void    ft_console_log(char *str) 
+void	ft_console_log(char *str) 
 {
-    int newFile;
-    
-    newFile = open("./projet.log", O_RDWR | O_CREAT | O_APPEND);
-    ft_putstr_fd(str, newFile);
+	int newFile;
+	
+	newFile = open("./projet.log", O_RDWR | O_CREAT | O_APPEND);
+	ft_putstr_fd(str, newFile);
+	close(newFile);
 }
 
-void    ft_console_log_char(char c)
+void	ft_console_log_char(char c)
 {
-    int newFile;
-    
-    newFile = open("./projet.log", O_RDWR | O_CREAT | O_APPEND);
-    ft_putchar_fd(c, newFile);
+	int newFile;
+	
+	newFile = open("./projet.log", O_RDWR | O_CREAT | O_APPEND);
+	ft_putchar_fd(c, newFile);
+	close(newFile);
 }
 
-void    ft_console_log_num(int num) 
+void	ft_console_log_num(int num) 
 {
-    int newFile;
-    
-    newFile = open("./projet.log", O_RDWR | O_CREAT | O_APPEND);
-    ft_putnbr_fd(num, newFile);
-
+	int newFile;
+	
+	newFile = open("./projet.log", O_RDWR | O_CREAT | O_APPEND);
+	ft_putnbr_fd(num, newFile);
+	close(newFile);
 }
 
-void    ft_console_log_line(const char *func, int line)
+void	ft_console_log_line(const char *func, int line)
 {
-    ft_console_log(ft_strjoin(func, ft_strjoin(" : ", ft_strjoin(ft_itoa(line), "\n"))));
+	ft_console_log(ft_strjoin(func, ft_strjoin(" : ", ft_strjoin(ft_itoa(line), "\n"))));
 }
 
-void    ft_console_log_clear(void)
+void	ft_console_log_clear(void)
 {
-    int newFile;
-    
-    newFile = open("./projet.log", O_RDWR | O_CREAT | O_TRUNC);
-    ft_putstr_fd("", newFile);
+	int newFile;
+	
+	newFile = open("./projet.log", O_RDWR | O_CREAT | O_TRUNC);
+	ft_putstr_fd("", newFile);
+	close(newFile);
 }
