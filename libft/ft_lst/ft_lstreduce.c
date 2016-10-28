392df56f631a8fd35641eef55deff66e71c9d45d
eebersol <eoduard.ebersoldt@gmail.com>
2016-10-28 18:25:39 +0200

    merge

--- libft/ft_lst/ft_lstreduce.c
+++ libft/ft_lst/ft_lstreduce.c
@@ -21,16 +21,11 @@ t_list	*ft_lstreduce(t_list *lst, int (*f)(t_list *elem))
     res = NULL;
 	while (lst)
 	{
-        ft_console_log("ft_lstreduce : 1\n");
 		if (f(lst) && lst != NULL)
         {
-            ft_console_log("ft_lstreduce : 2\n");
             ft_lstaddend(&res, lst);
-            ft_console_log("ft_lstreduce : 3\n");
         }
         lst = lst->next;
-        ft_console_log("ft_lstreduce : 4\n");
 	}
-    ft_console_log("ft_lstreduce : 5\n");
 	return (res);
 }
