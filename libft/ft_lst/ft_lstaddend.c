10920cbe724fe7bfa35d173571c0e542664cfdb0
qdequele <github@dequelen.me>
2016-10-31 18:33:19 +0100

    finish export

--- libft/ft_lst/ft_lstaddend.c
+++ libft/ft_lst/ft_lstaddend.c
@@ -6,7 +6,7 @@
 /*   By: qdequele <qdequele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/07 10:14:23 by qdequele          #+#    #+#             */
-/*   Updated: 2016/10/31 17:15:04 by qdequele         ###   ########.fr       */
+/*   Updated: 2016/10/31 18:23:54 by qdequele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,12 @@ void	ft_lstaddend(t_list **alst, t_list *new)
 	t_list	*list;
 
 	list = *alst;
-	__DEBUG__
 	if ((*alst == NULL || (*alst)->content == NULL )&& new)
-	{
-		__DEBUG__
 		*alst = new;
-	}
 	else
 	{
-		__DEBUG__
 		while (list->next)
-		{
-			__DEBUG__
 			list = list->next;
-		}
-		__DEBUG__
 		list->next = new;
-		__DEBUG__
 	}
 }
--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: qdequele <qdequele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/02 15:21:13 by qdequele          #+#    #+#             */
-/*   Updated: 2016/10/31 13:34:17 by qdequele         ###   ########.fr       */
+/*   Updated: 2016/10/31 18:32:40 by qdequele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,28 @@
 
 int			builtins_export(t_list **env, char **cmds)
 {
+	int 	i;
+	char	*value;
+
 	(void)env;
-	(void)cmds;
+	i = 1;
+	if (!cmds[i])
+	{
+		ft_putstr_c(RED, "export: Need an argument.\n");
+		return (1);
+	}
+	while (cmds[i])
+	{
+		value = vars_get(g_vars, cmds[i]);
+		if (ft_strlen(value) == 0)
+		{
+			ft_putstr_c(RED, "export: Impossible to find ");
+			ft_putstr_c(RED, cmds[i]);
+			ft_putstr_c(RED, "\n");
+		}
+		else
+			vars_add_or_modify(vars_recover(), cmds[i], value);
+		i++;
+	}
 	return (0);
 }
