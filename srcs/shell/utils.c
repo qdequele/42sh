2c1e8653cf7cba1ba780ceeaa663922c26a06a86
eebersol <eoduard.ebersoldt@gmail.com>
2016-11-03 16:33:37 +0100

    Fix $var sekk to g_env && g_vars

--- srcs/shell/utils.c
+++ srcs/shell/utils.c
@@ -15,6 +15,7 @@
 char    *replace_vars(char *line)
 {
     char    **cmd;
+    char    *tmp;
     int     i;
     t_shell	*shell;
 
@@ -23,12 +24,15 @@ char    *replace_vars(char *line)
     shell = recover_shell();
     while(cmd[i])
     {
+        tmp = cmd[i];
         if (cmd[i][0] == '$')
         {
             if (cmd[i][1] == '?' && cmd[i][2] == '\0')
                 cmd[i] = ft_itoa(shell->last_exit_code);
+            else if (!cmd[i])
+                cmd[i] = env_get(g_env, ft_strdup(&tmp[1]));
             else
-                cmd[i] = env_get(g_env, ft_strdup(&cmd[i][1]));
+                cmd[i] = env_get(g_vars, ft_strdup(&tmp[1]));
         }
         i++;
     }
