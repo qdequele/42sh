504eb78d1c951c84d09a86861cd26f87c37377ee
qdequele <github@dequelen.me>
2016-11-06 15:40:15 +0100

    remove unused params

--- srcs/parser/get_cmd_env.c
+++ srcs/parser/get_cmd_env.c
@@ -18,11 +18,8 @@ char	*get_cmd_env(char *cmd, char *var_raw)
 	char	*var;
 	char	*env_value;
 	int		i;
-	t_shell	*shell;
 
-	
 	env_value = NULL;
-	shell = recover_shell();
 	*var_raw = '\0';
 	i = 1;
 	while (ft_isalnum(var_raw[i]) || var_raw[i] == '?' || var_raw[i] == '_')
