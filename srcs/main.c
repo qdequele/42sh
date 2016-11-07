2114345656fe7944468b42daea61a26e2690aa35
Edouard EBERSOLDT <eebersol@e1r8p20.42.fr>
2016-11-07 19:08:05 +0100

    +1 to lst_add shell history

--- srcs/main.c
+++ srcs/main.c
@@ -88,7 +88,7 @@ void			shell_start(void)
 			reset_term();
 			process_input(line);
 			ft_lstadd_at(&shell->history,
-				ft_lstnew(line, sizeof(char) * ft_strlen(line)), shell->history_index);
+				ft_lstnew(line, sizeof(char) * ft_strlen(line) + 1), shell->history_index);
 			shell->history_index++;
 			ft_lstdel(&shell->prompt->line, free_char);
 			ignore_major_signals();
