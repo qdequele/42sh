0f04409567f17669d153e3823e3a3730507ce7c8
Edouard EBERSOLDT <eebersol@e1r4p7.42.fr>
2016-11-09 14:54:00 +0100

    add var prompt->end_cpy

--- srcs/prompt/init.c
+++ srcs/prompt/init.c
@@ -22,6 +22,7 @@ t_prompt	*init_prompt(void)
 	prompt->p_length = get_current_folder_length();
 	prompt->i_copy = 0;
 	prompt->flag_cut = 0;
+	prompt->end_cpy = 0;
 	prompt->str_cpy = NULL;
 	prompt->cut_len = 0;
 	prompt->quote_type = 0;
