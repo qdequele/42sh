04199ef1031ad57955553dce744f751bbb1e4c4a
eeberol <edouard.ebersoldt@gmail.com>
2016-11-21 21:02:55 +0100

    add export

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -12,6 +12,41 @@
 
 #include <ft_sh.h>
 
+
+static	int	is_formated(char *str)
+{
+	int	i;
+	char 	*p_key;
+
+	i = 0;
+	if ((p_key = ft_strchr(str, '=')) != NULL)
+		str = ft_strsub(str, 0, ft_strlen(str) - ft_strlen(p_key));
+	if (!ft_isalpha(str[i]) && str[i] != '_')
+		return (-1);
+	i++;
+	while (str[i])
+	{
+		if (!ft_isalpha(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	if (p_key)
+		free(str);
+	return (1);
+}
+
+static	int	is_error(char **cmds)
+{
+	if (is_formated(cmds[1]) == -1)
+	{
+		ft_putstr_fd("export: ", 2);
+		ft_putstr_fd(cmds[1], 2);
+		ft_putendl_fd(": not valid identifier", 2);
+		return (1);
+	}
+	return (0);
+}
+
 static 	void lets_export(char **cmds, int i)
 {
 	char 	*value;
@@ -25,13 +60,16 @@ static 	void lets_export(char **cmds, int i)
 			p_key = ft_strsub(cmds[i], 0, ft_strlen(cmds[i]) - (ft_strlen(p_value)));
 			p_value++;
 			vars_add_or_modify(&g_g_vars, p_key, p_value);
-			vars_add_or_modify(&g_env, p_key, p_value);
+			env_add_or_modify(&g_env, p_key, p_value);
+			vars_add_or_modify(&g_export, p_key, p_value);
 			free(p_key);
 		}
-		else if ((value = vars_get(g_l_vars, cmds[i])) != NULL)
+		else if ((value = vars_get(g_l_vars, cmds[i])) != NULL ||
+			(value = vars_get(g_export, cmds[i])) != NULL)
 		{
 			env_add_or_modify(&g_env, cmds[i], value);
 			vars_add_or_modify(&g_g_vars, cmds[i], value);
+			vars_add_or_modify(&g_export, cmds[i], value);
 		}
 		else if (!value)
 			vars_add_or_modify(&g_export, cmds[i], "");
@@ -65,6 +103,8 @@ int 	builtins_export(t_list **env, char **cmds)
 		display_export_list(g_export);
 		return (1);
 	}
+	if (is_error(cmds))
+		return (0);
 	lets_export(cmds, i);
 	return (1);
 
